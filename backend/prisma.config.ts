e112b3c2b7ef6ecb41e4a3ab69442879998cee9f
unknown <thalyson.kauan7@gmail.com>
2026-08-30 16:16:51 -0300

    fix: prisma db push configuration

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 import { defineConfig } from '@prisma/config'
 
 export default defineConfig({
   earlyAccess: true,
-  migrate: {
+  datasource: {
     url: process.env.DATABASE_URL,
   },
 })
