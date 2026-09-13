cce5028bc00990b6dfc9f20511177ea14af7a105
victorsouza00 <victorsouza183@gmail.com>
2026-09-13 20:08:15 -0300

    feat(frontend): novo método no serviços de login recoveryPasswordRequest

--- frontend/src/features/auth/api/authService.ts
+++ frontend/src/features/auth/api/authService.ts
@@ -151,3 +151,24 @@ export async function logoutRequest(): Promise<void> {
     console.warn('Falha na comunicação de logout com o backend:', error);
   }
 }
+
+export async function recoveryPasswordRequest(email:string): Promise<string> {
+    try {
+      const response = await apiClient.post<{ message: string }>("/auth/forgot-password", { email });
+      return response.data.message || "Se este e‑mail estiver cadastrado, você receberá um link de recuperação.!";
+    } catch (error) {
+      console.warn('Falha na comunicação de recoveryPassword com o backend:', error);
+
+      // 3. Mantém a sua verificação personalizada de erro
+      if (error instanceof AuthError) {
+        throw error;
+      }
+
+      // 4. Trata o erro do Axios para extrair a mensagem do backend
+      const axiosError = error as AxiosError<{ message?: string; error?: string }>;
+      const errorMessage = axiosError.response?.data?.message || axiosError.response?.data?.error || "Erro desconhecido ao recuperar senha.";
+
+      // 5. Lança o erro para ser capturado pela tela (UI) que chamou a função
+      throw new Error(errorMessage);
+    }
+}
